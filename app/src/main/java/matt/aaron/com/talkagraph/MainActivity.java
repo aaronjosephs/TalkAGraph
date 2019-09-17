9b3e9c63770941340d2e0841f136aca5c6cf7b89
aaronjosephs <ajoseph4@binghamton.edu>
2019-09-16 23:07:56 -0700

    Sort of got it working but it's not showing speaker diarization results

--- app/src/main/java/matt/aaron/com/talkagraph/MainActivity.java
+++ app/src/main/java/matt/aaron/com/talkagraph/MainActivity.java
@@ -1,216 +1,190 @@
 package matt.aaron.com.talkagraph;
 
-import android.media.MediaRecorder;
-import android.media.MediaRecorder.AudioEncoder;
-import android.media.MediaRecorder.AudioSource;
-import android.media.MediaRecorder.OutputFormat;
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.api.gax.rpc.BidiStream;
-import com.google.cloud.speech.v1p1beta1.RecognitionAudio;
-import com.google.cloud.speech.v1p1beta1.RecognitionConfig;
-import com.google.cloud.speech.v1p1beta1.RecognitionConfig.AudioEncoding;
-import com.google.cloud.speech.v1p1beta1.RecognizeResponse;
-import com.google.cloud.speech.v1p1beta1.SpeechClient;
 import com.google.cloud.speech.v1p1beta1.SpeechRecognitionAlternative;
-import com.google.cloud.speech.v1p1beta1.SpeechRecognitionResult;
-import com.google.cloud.speech.v1p1beta1.StreamingRecognitionConfig;
-import com.google.cloud.speech.v1p1beta1.StreamingRecognitionResult;
-import com.google.cloud.speech.v1p1beta1.StreamingRecognizeRequest;
-import com.google.cloud.speech.v1p1beta1.StreamingRecognizeResponse;
-import com.google.cloud.speech.v1p1beta1.WordInfo;
-import com.google.protobuf.ByteString;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-  MediaRecorder mediaRecorder;
-  String fileName = "./resources/audio.raw";
+  private static final int REQUEST_RECORD_AUDIO_PERMISSION = 1;
+  private static final String TAG = "MainActivity";
+  final Map<Integer, Integer> speakerMap = new HashMap<>();
+  int totalWordCount = 0;
+  private SpeechService speechService;
+  private VoiceRecorder voiceRecorder;
+  private final SpeechService.Listener speechServiceListener =
+      new SpeechService.Listener() {
+        @Override
+        public void onSpeechRecognized(final SpeechRecognitionAlternative alternative,
+            final boolean isFinal) {
+          if (isFinal) {
+            voiceRecorder.dismiss();
+          }
+
+          totalWordCount += alternative.getWordsCount();
+          alternative.getWordsList().forEach(entry -> {
+            int tag = entry.getSpeakerTag();
+            if (speakerMap.containsKey(tag)) {
+              speakerMap.put(tag, speakerMap.get(tag) + 1);
+            } else {
+              speakerMap.put(tag, 1);
+            }
+          });
+          runOnUiThread(MainActivity.this::updatePercents);
+        }
+      };
+  private final ServiceConnection serviceConnection = new ServiceConnection() {
+
+    @Override
+    public void onServiceConnected(ComponentName componentName, IBinder binder) {
+      speechService = SpeechService.from(binder);
+      speechService.addListener(speechServiceListener);
+      // mStatus.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName componentName) {
+      speechService = null;
+    }
+
+  };
+  private final VoiceRecorder.Callback voiceRecorderCallback = new VoiceRecorder.Callback() {
+
+    @Override
+    public void onVoiceStart() {
+      // showStatus(true);
+      if (speechService != null) {
+        Log.e(TAG, "Start recognizing");
+        speechService.startRecognizing(voiceRecorder.getSampleRate());
+      }
+    }
+
+    @Override
+    public void onVoice(byte[] data, int size) {
+      if (speechService != null) {
+        speechService.recognize(data, size);
+      }
+    }
+
+    @Override
+    public void onVoiceEnd() {
+      // showStatus(false);
+      if (speechService != null) {
+        speechService.finishRecognizing();
+      }
+    }
+
+  };
 
   private TextView speaker1;
   private TextView speaker2;
   private TextView speaker3;
   private TextView speaker4;
 
-  private static RecognitionConfig buildDiarizationConfig() {
-    return RecognitionConfig.newBuilder()
-        // TODO the only encodings they have in common
-        .setEncoding(AudioEncoding.AMR_WB) // LINEAR_16
-        .setSampleRateHertz(16000)
-        .setLanguageCode("en-US")
-        // This is just the esimated number of speakers in the convo
-        .setDiarizationSpeakerCount(4)
-        .setEnableSpeakerDiarization(true)
-        // Some newer api that doesn't seem to be finished yet
-        // .setDiarizationConfig(SpeakerDiarizationConfig.newBuilder().setEnableSpeakerDiarization(true).setMaxSpeakerCount()build())
-        .build();
-  }
-
-  private static StreamingRecognizeRequest buildStreamingRequest(ByteString audioBytes) {
-    StreamingRecognitionConfig streamingConfig = StreamingRecognitionConfig.newBuilder()
-        .setConfig(buildDiarizationConfig()).build();
-    return StreamingRecognizeRequest.newBuilder().setAudioContent(audioBytes)
-        .setStreamingConfig(streamingConfig).build();
-  }
-
-  private static RecognizeResponse getRecognizeResponse(RecognitionConfig config,
-      ByteString audio) {
-    try (SpeechClient speechClient = SpeechClient.create()) {
-      // Get the contents of the local audio file
-      RecognitionAudio recognitionAudio =
-          RecognitionAudio.newBuilder().setContent(audio).build();
-
-      // Perform the transcription request
-      return speechClient.recognize(config, recognitionAudio);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  //TODO streaming stuff
-  // get normal working first
-  private static void unused() {
-    // Instantiates a client
-    try (SpeechClient speechClient = SpeechClient.create()) {
-      // Reads the audio file into memory
-      Path path = Paths.get("");
-      byte[] data = Files.readAllBytes(path);
-      ByteString audioBytes = ByteString.copyFrom(data);
-
-      StreamingRecognizeRequest streamingRequest = buildStreamingRequest(audioBytes);
-
-      // Builds the sync recognize request
-
-      // StreamingRecognitionConfig streamingConfig = StreamingRecognitionConfig.newBuilder().setConfig(config).build();
-      //
-      // StreamingRecognizeRequest request = StreamingRecognizeRequest.newBuilder().setStreamingConfig(streamingConfig).build();
-
-      // RecognitionAudio audio = RecognitionAudio.newBuilder()
-      //     .setContent(audioBytes)
-      //     .build();
-
-      // Performs speech recognition on the audio file
-      // RecognizeResponse response = speechClient.recognize(config, audio);
-      // List<SpeechRecognitionResult> results = response.getResultsList();
-
-      BidiStream<StreamingRecognizeRequest, StreamingRecognizeResponse> observer = speechClient
-          .streamingRecognizeCallable().call();
-
-      observer.send(streamingRequest);
-
-      for (StreamingRecognizeResponse response : observer) {
-        for (StreamingRecognitionResult result : response.getResultsList()) {
-          for (WordInfo wordInfo : result.getAlternatives(0).getWordsList()) {
-
-            wordInfo.getSpeakerTag(); // Assign the int to a name later
-          }
-        }
-      }
-
-      // for (SpeechRecognitionResult result : results) {
-      //   // There can be several alternative transcripts for a given chunk of speech. Just use the
-      //   // first (most likely) one here.
-      //   SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-      //   System.out.printf("Transcription: %s%n", alternative.getTranscript());
-      //   alternative.getWords(1).getSpeakerTag();
-      // }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    // Record jame's stupid voice
-    mediaRecorder = new MediaRecorder();
-    mediaRecorder.setAudioSource(AudioSource.MIC);
-    // Is this the right format for cloud to speech?
-    mediaRecorder.setOutputFormat(OutputFormat.MPEG_2_TS);
-    mediaRecorder.setOutputFile(new File(fileName));
-    // TODO what encoder to use????
-    mediaRecorder.setAudioEncoder(AudioEncoder.AMR_WB);
-    try {
-      mediaRecorder.prepare();
-    } catch (IOException e) {
-      // TODO handle exception better
-      e.printStackTrace();
-    }
-
     setContentView(R.layout.activity_main);
+
+    // Record james' stupid voice
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+        != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 20);
+    }
 
     speaker1 = findViewById(R.id.speaker1);
     speaker2 = findViewById(R.id.speaker2);
     speaker3 = findViewById(R.id.speaker3);
     speaker4 = findViewById(R.id.speaker4);
+
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+
+    // Prepare Cloud Speech API
+    bindService(new Intent(this, SpeechService.class), serviceConnection, BIND_AUTO_CREATE);
+
+    // Start listening to voices
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+        == PackageManager.PERMISSION_GRANTED) {
+      //   startVoiceRecorder();
+      // } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+      //     Manifest.permission.RECORD_AUDIO)) {
+      //   showPermissionMessageDialog();
+    } else {
+      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
+          REQUEST_RECORD_AUDIO_PERMISSION);
+    }
+  }
+
+  @Override
+  protected void onStop() {
+    // Stop listening to voice
+    if (voiceRecorder != null) {
+      voiceRecorder.stop();
+      voiceRecorder = null;
+    }
+    // Stop Cloud Speech API
+    speechService.removeListener(speechServiceListener);
+    unbindService(serviceConnection);
+    speechService = null;
+
+    super.onStop();
   }
 
   public void startRecording(View view) {
-    mediaRecorder.start();
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+        != PackageManager.PERMISSION_GRANTED) {
+      //  maybe show a toast here or something
+      return;
+    }
+    if (voiceRecorder != null) {
+      voiceRecorder.stop();
+    }
+    voiceRecorder = new VoiceRecorder(voiceRecorderCallback);
+    voiceRecorder.start();
   }
 
   public void stopRecording(View view) {
-    mediaRecorder.stop();
-    Path path = Paths.get("");
-    byte[] data;
-    try {
-      data = Files.readAllBytes(path);
-
-      ByteString audioBytes = ByteString.copyFrom(data);
-      RecognizeResponse response = getRecognizeResponse(buildDiarizationConfig(), audioBytes);
-
-      updatePercents(response);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+    if (voiceRecorder != null) {
+      voiceRecorder.stop();
+      voiceRecorder = null;
     }
-
   }
 
-  private void updatePercents(RecognizeResponse response) {
+  private void updatePercents() {
     // we're just doing word count for now we can do elapsed time later
-    final Map<Integer, Integer> speakerMap = new HashMap<>();
-    int totalWordCount = 0;
-    for (SpeechRecognitionResult result : response.getResultsList()) {
-      // There can be several alternative transcripts for a given chunk of speech. Just
-      // use the first (most likely) one here.
-      SpeechRecognitionAlternative alternative = result.getAlternatives(0);
-      // The words array contains the entire transcript up until that point.
-      // Referencing the last spoken word to get the associated Speaker tag
-
-      totalWordCount += alternative.getWordsCount();
-      alternative.getWordsList().forEach(entry -> {
-        int tag = entry.getSpeakerTag();
-        if (speakerMap.containsKey(tag)) {
-          speakerMap.put(tag, speakerMap.get(tag) + 1);
-        } else {
-          speakerMap.put(tag, 1);
-        }
-      });
-    }
     if (speakerMap.size() > 4) {
       Toast toast = Toast.makeText(getApplicationContext(),
           "More than 4 speakers detected",
           Toast.LENGTH_SHORT);
       toast.show();
+    } else if (speakerMap.isEmpty()) {
+      return;
     }
     // TODO make this more dynamic to support arbitrary speaker count
     speaker1.setText(String.format("Speaker %d spoke %f percent", 1,
-        ((double) speakerMap.get(1) / totalWordCount) * 100));
+        ((double) speakerMap.getOrDefault(1, 0) / totalWordCount) * 100));
     speaker2.setText(String.format("Speaker %d spoke %f percent", 2,
-        ((double) speakerMap.get(2) / totalWordCount) * 100));
+        ((double) speakerMap.getOrDefault(2, 0) / totalWordCount) * 100));
     speaker3.setText(String.format("Speaker %d spoke %f percent", 3,
-        ((double) speakerMap.get(3) / totalWordCount) * 100));
+        ((double) speakerMap.getOrDefault(3, 0) / totalWordCount) * 100));
     speaker4.setText(String.format("Speaker %d spoke %f percent", 4,
-        ((double) speakerMap.get(4) / totalWordCount) * 100));
+        ((double) speakerMap.getOrDefault(4, 0) / totalWordCount) * 100));
   }
 }
---
+++ app/src/main/java/matt/aaron/com/talkagraph/SpeechService.java
@@ -0,0 +1,215 @@
+package matt.aaron.com.talkagraph;
+
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.rpc.ClientStream;
+import com.google.api.gax.rpc.ResponseObserver;
+import com.google.api.gax.rpc.StreamController;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.speech.v1p1beta1.RecognitionConfig;
+import com.google.cloud.speech.v1p1beta1.SpeechClient;
+import com.google.cloud.speech.v1p1beta1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1p1beta1.SpeechSettings;
+import com.google.cloud.speech.v1p1beta1.StreamingRecognitionConfig;
+import com.google.cloud.speech.v1p1beta1.StreamingRecognitionResult;
+import com.google.cloud.speech.v1p1beta1.StreamingRecognizeRequest;
+import com.google.cloud.speech.v1p1beta1.StreamingRecognizeResponse;
+import com.google.protobuf.ByteString;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
+// https://github.com/GoogleCloudPlatform/android-docs-samples/blob/master/speech/SpeechRecognitionClient/app/src/main/java/com/google/cloud/examples/speechrecognition/MainActivity.kt
+// get rid of speech grpc by using that
+public class SpeechService extends Service {
+
+  private static final String TAG = "SpeechService";
+  private final SpeechBinder mBinder = new SpeechBinder();
+  private final ArrayList<Listener> mListeners = new ArrayList<>();
+  StreamingRecognizeRequest request;
+  private ClientStream<StreamingRecognizeRequest> clientStream;
+  private SpeechClient speechClient;
+  private ConcurrentLinkedQueue<StreamingRecognizeRequest> requestsQueue;
+  private AtomicBoolean stoppedRecording = new AtomicBoolean(false);
+
+  public static SpeechService from(IBinder binder) {
+    return ((SpeechBinder) binder).getService();
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    InputStream stream = this.getResources().openRawResource(R.raw.credentials);
+    try {
+      GoogleCredentials credential = GoogleCredentials.fromStream(stream);
+      CredentialsProvider credentialsProvider = new CredentialsProvider() {
+        public Credentials getCredentials() {
+          return credential;
+        }
+      };
+      speechClient = SpeechClient
+          .create(SpeechSettings.newBuilder().setCredentialsProvider(credentialsProvider).build());
+      requestsQueue = new ConcurrentLinkedQueue<>();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+  }
+
+  private String getDefaultLanguageCode() {
+    final Locale locale = Locale.getDefault();
+    final StringBuilder language = new StringBuilder(locale.getLanguage());
+    final String country = locale.getCountry();
+    if (!TextUtils.isEmpty(country)) {
+      language.append("-");
+      language.append(country);
+    }
+    return language.toString();
+  }
+
+  @Nullable
+  @Override
+  public IBinder onBind(Intent intent) {
+    return mBinder;
+  }
+
+  public void addListener(@NonNull Listener listener) {
+    mListeners.add(listener);
+  }
+
+  public void removeListener(@NonNull Listener listener) {
+    mListeners.remove(listener);
+  }
+
+  /**
+   * Starts recognizing speech audio.
+   *
+   * @param sampleRate The sample rate of the audio.
+   */
+  public void startRecognizing(int sampleRate) {
+    request = StreamingRecognizeRequest.newBuilder()
+        .setStreamingConfig(StreamingRecognitionConfig.newBuilder()
+            .setConfig(RecognitionConfig.newBuilder()
+                .setLanguageCode(getDefaultLanguageCode())
+                .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+                .setSampleRateHertz(sampleRate)
+                .setDiarizationSpeakerCount(4) // estimated count?
+                .setEnableSpeakerDiarization(true)
+                .build())
+            .setInterimResults(true)
+            .setSingleUtterance(false)
+            .build())
+        .build();
+
+    ResponseObserver<StreamingRecognizeResponse> observer =
+        new ResponseObserver<StreamingRecognizeResponse>() {
+          @Override
+          public void onStart(StreamController controller) {
+            // no-op
+          }
+
+          @Override
+          public void onResponse(StreamingRecognizeResponse response) {
+            SpeechRecognitionAlternative alternative = null;
+            Log.d(TAG, response.toString());
+            boolean isFinal = false;
+            if (response.getResultsCount() > 0) {
+              // TODO: Should we iterate through all of them, not sure, seems like first is final?
+              final StreamingRecognitionResult result = response.getResults(0);
+              isFinal = result.getIsFinal();
+              if (result.getAlternativesCount() > 0) {
+                alternative = result.getAlternatives(0);
+              }
+            }
+            if (alternative != null) {
+              for (Listener listener : mListeners) {
+                listener.onSpeechRecognized(alternative, isFinal);
+              }
+            }
+          }
+
+          @Override
+          public void onComplete() {
+          }
+
+          @Override
+          public void onError(Throwable t) {
+            Log.i(TAG, "API completed.");
+          }
+        };
+    clientStream = speechClient.streamingRecognizeCallable().splitCall(observer);
+    clientStream.send(request);
+  }
+
+  /**
+   * Recognizes the speech audio. This method should be called every time a chunk of byte buffer is
+   * ready.
+   *
+   * @param data The audio data.
+   * @param size The number of elements that are actually relevant in the {@code data}.
+   */
+  public void recognize(byte[] data, int size) {
+    clientStream.send(StreamingRecognizeRequest.newBuilder()
+        .setAudioContent(ByteString.copyFrom(data, 0, size))
+        .build());
+  }
+
+  /**
+   * Finishes recognizing speech audio.
+   */
+  public void finishRecognizing() {
+    stoppedRecording.set(true);
+    // clientStream.send(request);
+  }
+
+  public interface Listener {
+
+    /**
+     * Called when a new piece of text was recognized by the Speech API.
+     *
+     * @param text The text.
+     * @param isFinal {@code true} when the API finished processing audio.
+     */
+    void onSpeechRecognized(SpeechRecognitionAlternative text, boolean isFinal);
+
+  }
+
+  private class SpeechBinder extends Binder {
+
+    SpeechService getService() {
+      return SpeechService.this;
+    }
+  }
+}
